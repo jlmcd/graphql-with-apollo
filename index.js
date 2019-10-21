de62c1214700582beca249e5ca91d143bfa887d9
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-10-21 15:00:57 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const listings = [
+  { id: '001', title: 'Large Ensuite Condo', city: 'Toronto' },
+  { id: '002', title: 'Beverly Hills Mansion', city: 'Los Angeles' },
+  { id: '003', title: 'Small Chic Bedroom', city: 'Dubai' }
+]
